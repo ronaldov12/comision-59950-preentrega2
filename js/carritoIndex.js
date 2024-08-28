d6071c55635e5d5793336651ef1233d0b2be8db5
ronaldoV <ronaldovillalba5373@gmail.com>
2024-08-28 12:48:22 -0300

    Corregido error en la ruta de imágenes

--- js/carritoIndex.js
+++ js/carritoIndex.js
@@ -3,7 +3,7 @@ const productos = [
     { id: 1, nombre: "Reloj Tommy Hilfiger", precio: 359, imagen: 'img/hollyfinger.webp' },
     { id: 2, nombre: "Reloj Tissot Everytime Gent", precio: 549, imagen: 'img/relojtissot.webp' },
     { id: 3, nombre: "Reloj Tissot Seastar", precio: 764, imagen: 'img/relojtissotseastar.webp' },
-    { id: 4, nombre: "Reloj Swatch Ashbayang", precio: 130, imagen: '/img/swatchAshbayang.webp' },
+    { id: 4, nombre: "Reloj Swatch Ashbayang", precio: 130, imagen: 'img/swatchAshbayang.webp' },
 ];
 
 
